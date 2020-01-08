7b8c75f05d68de6fd42446b3d63ee3a37218fbcb
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2020-01-08 13:22:54 +1300

    render timetable start time

--- src/components/timetable.js
+++ src/components/timetable.js
@@ -14,6 +14,14 @@ const Timetable = props => {
 
 	const getClassWeekday = date => weekdays[new Date(date).getDay() -1]
 
+	const getClassTime = date => {
+		date = new Date(date)
+		var hour = date.getHours() 
+		var minute = date.getMinutes() == 0 ? `00` : date.getMinutes()
+		console.log(date.getMinutes())
+		return hour <= 12 ? `${hour}:${minute}am` : `${hour - 12}:${minute}pm`
+	}
+
 	const addClassToTimetable = yogaClass => timetable[getClassWeekday(yogaClass.startTime)].push(yogaClass)
 
 	const sortClasses = classes => {
@@ -29,6 +37,11 @@ const Timetable = props => {
 			return (
 				<FlexItem width="14.285%">
 					<h2>{weekday}</h2>
+					{
+						timetable[weekday].map((yogaClass => {
+							return <p>{getClassTime(yogaClass.startTime)}</p>
+						}))
+					}
 				</FlexItem>
 			)
 		}))
