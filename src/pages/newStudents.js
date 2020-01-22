06ecad86f30a5cae0bb98c41ff900fedc56a78d7
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2020-01-22 13:29:27 +1300

    add new student blurb

--- src/pages/newStudents.js
+++ src/pages/newStudents.js
@@ -6,6 +6,7 @@ import Img from "gatsby-image"
 import { ContentContainer, FlexContainer, InnerContainer } from "../styled-components/layout"
 import Quote from "../components/quote"
 import SingleTextColumn from '../components/singleTextColumn'
+import TripleTextColumn from "../components/tripleTextColumn"
 
 
 
@@ -13,15 +14,25 @@ class newStudentsPage extends React.Component {
   render() {
     const { data } = this.props
     const classTypes = data.allContentfulClassType.edges
-    console.log(classTypes)
+    const page = data.contentfulPage
+
+    console.log(data)
+
     return (
       <Layout>
         <SEO title="Class Types" />
         <ContentContainer>
           <InnerContainer>
-            <h1>Class Levels</h1>
+            <h1>New Students</h1>
           </InnerContainer>
           <FlexContainer>
+            {
+              page.tripleTextColumns.map(tripleTextColumn => {
+                return (
+                  <TripleTextColumn content={tripleTextColumn.content} ></TripleTextColumn>
+                )
+              })
+            }
             {classTypes.map(classType => {
               return (
                 <SingleTextColumn title={classType.node.longName} content={classType.node.description.json} />
@@ -38,6 +49,16 @@ export default newStudentsPage
 
 export const pageQuery = graphql`
   query {
+    
+    contentfulPage(contentful_id: {eq: "24GOqYDwtQVqrMCVAZrzfS"}){
+      title
+      tripleTextColumns {
+        content {
+          json
+        }
+      }
+    }
+
     allContentfulClassType(
       sort: {
         fields: [order]
